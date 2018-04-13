b714fab2c9a359e27ebc4dd86e0fa0f48286e9d3
Yordan Ivanov <Yordan Ivanov@DESKTOP-CJOO4H7>
2018-04-13 18:01:51 +0300

    Add project files.

---
+++ Krisko/Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Speech.Synthesis;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Krisko
+{
+    public partial class Form1 : Form
+    {
+        SpeechSynthesizer speechSynthesizerObj;
+        public Form1()
+        {
+            InitializeComponent();
+            speechSynthesizerObj = new SpeechSynthesizer();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if(textBox1.Text == string.Empty)
+            {
+                MessageBox.Show("Моля въведете текст!");
+            }
+            else
+            {
+                speechSynthesizerObj.Dispose();
+                speechSynthesizerObj = new SpeechSynthesizer();
+                var voices = speechSynthesizerObj;
+
+                speechSynthesizerObj.SelectVoice("Microsoft Zira Desktop");
+
+                speechSynthesizerObj.SpeakAsync(textBox1.Text);
+            }
+            
+        }
+    }
+}
